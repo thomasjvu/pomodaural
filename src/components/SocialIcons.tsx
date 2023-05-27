60d5c9170b20908a3a3acc64ce40179cdf859da2
Thomas Vu <thomasjvu@gmail.com>
2023-05-27 03:01:29 -0400

    add icons

---
+++ src/components/SocialIcons.tsx
@@ -0,0 +1,22 @@
+import { Link } from "react-router-dom";
+import { YoutubeFill, TwitterFill, SoundcloudFill } from "./Icons";
+
+const SocialIcons: React.FC = (): JSX.Element => {
+
+    return (
+        <div className="flex w-full gap-4 justify-end items-center font-mono uppercase py-20">
+            <span>Visit us on: </span>
+            <Link to="https://soundcloud.com/pomodaural" target="_blank">
+                <SoundcloudFill className="w-6 h-6"/>
+            </Link>
+            <Link to="https://twitter.com/pomodaural" target="_blank">
+                <TwitterFill className="w-6 h-6"/>
+            </Link>
+            <Link to="https://youtube.com/@pomodaural" target="_blank">
+                <YoutubeFill className="w-6 h-6"/>
+            </Link>
+        </div>
+    )
+}
+
+export default SocialIcons
--- src/components/ToggleMenu.tsx
+++ src/components/ToggleMenu.tsx
@@ -1,4 +1,5 @@
 import Logo from "./Logo";
+import SocialIcons from "./SocialIcons";
 
 interface ToggleMenuProps {
     isOpen: boolean;
@@ -15,34 +16,35 @@ const ToggleMenu: React.FC<ToggleMenuProps> = ({ isOpen, handleToggle }) => {
                 {isOpen && (
                     <div className="flex flex-col items-center gap-10 px-16 w-full">
                         <Logo />
-                        <h1 className="text-white font-display uppercase text-4xl leading-loose tracking-widest">
+                        <h1 className="text-justify font-display uppercase text-4xl leading-loose tracking-widest">
                             Pomodaural
                         </h1>
                         <div id="about" className="flex flex-col gap-2 text-white">
-                            <p>
+                            <p className="text-justify">
                                 Pomodaural is a Pomodoro Timer with built-in Binaural frequencies to help you completely
                                 focus on the task at hand.
                             </p>
-                            <h3 className="text-left text-xl font-bold uppercase font-display">What is Pomodoro?</h3>
-                            <p>
+                            <h3 className="text-justify text-xl font-bold uppercase font-display">What is Pomodoro?</h3>
+                            <p className="text-justify">
                                 The Pomodoro Technique is a simple yet effective method for maximizing focus and
                                 reducing distrations that works by setting time in a period of 25 minutes of work and 5
                                 minutes of break.
                             </p>
-                            <h3 className="text-left text-xl font-bold uppercase font-display">
+                            <h3 className="text-justify text-xl font-bold uppercase font-display">
                                 What are Binaural Beats?
                             </h3>
-                            <p>
+                            <p className="text-justify">
                                 This is paired with Binaural Beats and research is ongoing about how they can manipulate
                                 our brain states to achieve higher levels of focus and creativity depending on the
                                 specific frequency.
                             </p>
                             <h3 className="text-left text-xl font-bold uppercase font-display">How to Use this App</h3>
-                            <p>
+                            <p className="text-justify">
                                 To begin, press the play button with the preset that depicts the state you want to be
                                 in, and then press start! The timer will go on for the intended duration.
                             </p>
                         </div>
+                        <SocialIcons />
                     </div>
                 )}
             </div>
